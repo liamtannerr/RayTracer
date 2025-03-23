7fb0a18d56fa68c6afc2bf1933bc92b1d8ce1979
Liam Tanner <liamtanner@uvic.ca>
2025-03-23 15:31:32 -0700

    Progress bar added

--- main.cc
+++ main.cc
@@ -3,18 +3,21 @@
 int main (){
 
     //Create inital square image dimensions
-
     int imgWidth = 256;
     int imgHeight = 256;
 
+    //Create header for image file
     std::cout <<"P3\n" <<imgWidth << ' ' << imgHeight << "\n255\n";
 
+    //Fill the image row by row
     for(int j = 0; j < imgHeight; j++){
+        //Create a progress bar for long renders
+        std::clog << "\rScanlines remaining: " << (imgHeight - j) << ' ' << std::flush;
         for(int i = 0; i < imgWidth; i++){
 
             auto r = double(i) / (imgWidth - 1);
             auto g = double(j) / (imgHeight - 1);
-            auto b = 0.0;
+            auto b = 1.0;
 
             int ir = int(255.999 * r);
             int ig = int(255.999 * g);
@@ -23,4 +26,6 @@ int main (){
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                      \n";
 }
