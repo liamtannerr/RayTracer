82cfbf979065a34e27a7f3eb2a7760ccf53eb84c
Liam Tanner <liamtanner@uvic.ca>
2025-04-27 23:57:18 -0700

    Simple camera viewport implemented

--- main.cc
+++ main.cc
@@ -1,24 +1,60 @@
 #include <iostream>
 #include "vec3.h"
 #include "colour.h"
+#include "ray.h"
+
+
+colour ray_colour(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*colour(1.0, 1.0, 1.0) + a*colour(1.0, 0.7, 1.0);
+}
 
 int main (){
 
-    //Create inital square image dimensions
-    int imgWidth = 256;
-    int imgHeight = 256;
+    //image
+
+    auto aspect_ratio = 16.0/ 9.0;
+    int image_width = 400;
+
+    //Calculate the image height based on the aspect ratio and ensure it's at least 1
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    //Camera
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    //Can't just use aspect_ratio as image_width / image_height might not be exactly 16.0 / 9.0
+    auto viewport_width = viewport_height * (double(image_width / image_height));
+    auto camera_center = point3(0, 0, 0);
+
+    //Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    //Calculate the horizontal and vertical delta vectors from pixel to pixel
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    //Calculate the location of the upper left pixel
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - (viewport_u + viewport_v)/2;
+    auto pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v)/2;
 
     //Create header for image file
-    std::cout <<"P3\n" <<imgWidth << ' ' << imgHeight << "\n255\n";
+    std::cout <<"P3\n" <<image_width << ' ' << image_height << "\n255\n";
+    //Render
 
     //Fill the image row by row
-    for(int j = 0; j < imgHeight; j++){
+    for(int j = 0; j < image_height; j++){
         //Create a progress bar for long renders
-        std::clog << "\rScanlines remaining: " << (imgHeight - j) << ' ' << std::flush;
-        for(int i = 0; i < imgWidth; i++){
-            //Create pixel colours using vec3 class
-            auto pixel_colour = colour(double(i) / (imgWidth - 1), 0, double(j) / (imgHeight - 1));
-            //Write using colour.h
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for(int i = 0; i < image_width; i++){
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            colour pixel_colour = ray_colour(r);
             write_colour(std::cout, pixel_colour);
         }
     }
